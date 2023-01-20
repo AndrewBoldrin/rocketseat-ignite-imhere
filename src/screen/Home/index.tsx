dadeba6dd44e6bcc1911751f7d6dcccde359df9a
andrew boldrin <andrewboldrin@hotmail.com>
2023-01-20 12:23:55 -0300

    add form

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -10,14 +10,16 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Quinta, 19 de Janeiro de 2023</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }

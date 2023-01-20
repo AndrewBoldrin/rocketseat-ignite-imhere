6669796adac5dfad448c5cdd91a4412d6436749d
andrew boldrin <andrewboldrin@hotmail.com>
2023-01-20 19:47:12 -0300

    status bar styling

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
+import { StatusBar } from "react-native";
 import { Home } from "./src/screen/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
